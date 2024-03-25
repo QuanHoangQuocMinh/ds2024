108ff40bb19e6f26cde0bd9b45549b92d077db66
QuanHoangQuocMinh <quanhqm.bi12-363@st.usth.edu.vn>
2024-03-25 12:25:06 +0700

    lab1

---
+++ lab1/server/server_side.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to localhost and specific port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listening for connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // File receiving logic
+    FILE *received_file = fopen("received_file.txt", "wb");
+    if (received_file == NULL) {
+        perror("File cannot be opened");
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_received;
+    while ((bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+        fwrite(buffer, sizeof(char), bytes_received, received_file);
+    }
+
+    printf("File received successfully.\n");
+
+    fclose(received_file);
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
