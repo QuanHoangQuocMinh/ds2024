dfd6eedd8a67f333a15683bd152d1d1f46ef2da0
quanhoang191003 <quanhoang191003@gmail.com>
2024-04-21 12:58:52 +0700

    fix lab4

--- lab4/wordcount/wordcount.c
+++ lab4/wordcount/wordcount.c
@@ -3,23 +3,28 @@
 #include <string.h>
 #include <ctype.h>
 
-// Define the maximum length of a word
 #define MAX_WORD_LENGTH 100
 
 // Data structure for intermediate key-value pairs
-typedef struct {
+typedef struct
+{
     char key[MAX_WORD_LENGTH];
     int value;
 } KeyValuePair;
 
 // Mapper function
-void map(char *line, KeyValuePair *pairs, int *pair_count) {
+void map(char *line, KeyValuePair *pairs, int *pair_count)
+{
     char word[MAX_WORD_LENGTH];
     int i = 0;
-    while (*line) {
-        if (isalnum(*line)) {
+    while (*line)
+    {
+        if (isalnum(*line))
+        {
             word[i++] = tolower(*line);
-        } else if (i > 0) {
+        }
+        else if (i > 0)
+        {
             word[i] = '\0';
             // Add key-value pair to the array
             strcpy(pairs[*pair_count].key, word);
@@ -32,32 +37,40 @@ void map(char *line, KeyValuePair *pairs, int *pair_count) {
 }
 
 // Reducer function
-void reduce(KeyValuePair *pairs, int pair_count) {
+void reduce(KeyValuePair *pairs, int pair_count)
+{
     int i, j, total_count;
-    for (i = 0; i < pair_count; i++) {
+    for (i = 0; i < pair_count; i++)
+    {
         // Check if the word already exists in the array
         total_count = pairs[i].value;
-        for (j = i + 1; j < pair_count; j++) {
-            if (strcmp(pairs[i].key, pairs[j].key) == 0) {
+        for (j = i + 1; j < pair_count; j++)
+        {
+            if (strcmp(pairs[i].key, pairs[j].key) == 0)
+            {
                 total_count += pairs[j].value;
                 pairs[j].value = 0; // Mark as visited
             }
         }
         // Print the word and its count
-        if (pairs[i].value != 0) {
+        if (pairs[i].value != 0)
+        {
             printf("%s\t%d\n", pairs[i].key, total_count);
         }
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file.\n");
         return 1;
     }
@@ -67,7 +80,8 @@ int main(int argc, char *argv[]) {
     int pair_count = 0;
 
     // Map phase
-    while (fgets(line, sizeof(line), file)) {
+    while (fgets(line, sizeof(line), file))
+    {
         map(line, pairs, &pair_count);
     }
 
